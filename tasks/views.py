66d05236aff8f9b9038b98640c8608db4b97bf30
Misael <leasim3x@gmail.com>
2024-02-17 03:31:44 -0600

    Implementación del Signin

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 
@@ -33,3 +34,24 @@ def signup(request):
 
 def tasks(request):
     return render(request, 'tasks.html')
+
+def singout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+       return render(request, 'signin.html',{
+        'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        print('Solicitud de Inicio de sesion')
+        if user is None:
+            return render(request, 'signin.html',{
+                'form': AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
